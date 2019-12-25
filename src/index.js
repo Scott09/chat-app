0235e15a72682f20e913f12ee15dd5065a9c0669
Scott Appleton <scottappleton09@gmail.com>
2019-12-24 22:35:16 -0800

    Add nodemon dependancy, use middleware to server static files from public directory, render html at root URI

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const port = 3000 || process.env.PORT;
 const app = express();
 
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('index.html')
+})
+
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
