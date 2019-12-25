7a1f385be350d24fa406df891e805ec23044053e
Scott Appleton <scottappleton09@gmail.com>
2019-12-24 22:56:59 -0800

    Configure server to use http and bring in our express server. Add socketio into express app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const http = require('http');
 const port = 3000 || process.env.PORT;
 const app = express();
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
 
 app.use(express.static('public'));
 
+io.on('connection', () => {
+  console.log('new websocket connection')
+});
 
-app.get('/', (req, res) => {
-  res.render('index.html')
-})
 
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 });
